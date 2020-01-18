db4399675deadba446bb70cb2db136f10bbf533f
sjmars <sjmarsnc@gmail.com>
2020-01-18 09:17:17 -0500

    tests of a couple of different packages, fixed missing div when no current job

--- resumeGenerator.js
+++ resumeGenerator.js
@@ -2,6 +2,16 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 const axios = require("axios");
+const electron = require("electron");
+const proc = require('child_process'); 
+const child = proc.spawn(electron); 
+const convertFactory = require('electron-html-to'); 
+
+var totalStars = 0;  
+
+// const pdfpkg = require('html-pdf'); 
+
+
 // const github = require("github"); 
 
 const writeFileAsynch = util.promisify(fs.writeFile);  
@@ -16,11 +26,22 @@ inquirer.prompt([
         name: "githubname"
     }
 ]).then( function (answer) {
-    // const queryRepos= `https://api.github.com/users/${githubname}/repos?per_page=100`;
-    const queryUser = `https://api.github.com/users/${answer.githubname}`;  
+    const queryRepos= `https://api.github.com/users/${answer.githubname}/repos`;
+    const queryUser = `https://api.github.com/users/${answer.githubname}`; 
+    
+    
+    
+    axios.get(queryRepos).then (function (repos) {
+    
+        for (const repo of repos.data) {
+            totalStars += repo.stargazers_count;
+        }
+        console.log(repos.data); 
+        console.log("totalStars: ",totalStars); 
+    }); 
      axios.get(queryUser).then(function (userinfo) {
-        console.log(userinfo); 
-        const { name, avatar_url, company, blog, location, bio, public_repos, public_gists, followers, following } = userinfo;
+        console.log(userinfo.data); 
+        const { name, avatar_url, company, blog, location, bio, public_repos, public_gists, followers, following } = userinfo.data;
 
         var resumeContent = `
 <!DOCTYPE html>
@@ -29,11 +50,31 @@ inquirer.prompt([
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./resume.css">
     <title>Resume for ${name}</title>
 </head>
+
+<style>
+.mycards { 
+    background-color: midnightblue; 
+    color: white;
+}
+
+.jumbotron {
+    position: relative;
+    background-color: lavender;
+}
+
+#headshot { 
+    position: bottom 20%;  
+    width: 400px; 
+    height: 400px; 
+
+}
+
+</style>
 
 <body>
     <div class="container text-center">
@@ -41,37 +82,47 @@ inquirer.prompt([
             <h1>Hello! I'm ${name}</h1>
             <img src="${avatar_url}" id="headshot">`; 
         if (company !== null) {
-            resumeContent = resumeContent + `<h3 id="job">${company}</h3>`;
+            resumeContent = resumeContent + `
+            <h3 id="job">Currently at ${company}</h3>`;
         }
         resumeContent = resumeContent + `
-            <div class="row">
+            <div class="row justify-content-around">`;
+        if (location !== null) {
+            resumeContent = resumeContent + `
+            <span class="col col-2 col-md-3 justify-content-center">
+            <i class="fa fa-2x fa-location-arrow"></i>
+            <span id="location">${location}</span>
+            </span>`; 
+        }
+        resumeContent = resumeContent + `    
                 <span class="col col-2 col-md-3 justify-content-center">
-                    <i class="fa fa-2x fa-location-arrow"></i>
-                    <span id="location">${location}</span>
-                </span>
-                <span class="col col-2 col-md-3">
                     <i class="fa fa-2x fa-github"></i>
                     <span id="github"><a href="https://github.com/${name}">${name}</a></span>
-                </span>
-                <span class="col col-2 col-md-3 justify-content-center">
+                </span>`;
+        if (blog !== null) { 
+            resumeContent = resumeContent + `
+            <span class="col col-2 col-md-3 justify-content-center">
                     <i class="fa fa-2x fa-desktop"></i>
                     <span id="website">
-                        <a href="www.linkedin.com/in/susan-marshall-11367715">Susan-Marshall</a>
+                        <a href="${blog}">Website</a>
                     </span>
-                </span>
-            </div>
+                </span>`;
+        }
+        resumeContent = resumeContent + 
+        `    </div>
         </div>
-    </div>`;
+    </div>
+    <div class="container w-75 ">
+    `;
     if (bio !== null) {
-        resumeContent = resumeContent +
-   `    <div class="container w-75 ">
+        resumeContent = resumeContent + `
         <div class="row justify-content-center"> 
             <h3 class="col-12 text-center" id = "bio"> ${bio}</h3>
         </div>`; 
     }
-    resumeContent = resumeContent + 
-    `    <div class="row justify-content-center">
-            <div class="col-12 col-md-5">
+    resumeContent = resumeContent + `    
+          <div class="row justify-content-center">
+            <div class="col-xs-4">
                 <div class="card m-3 mycards">
                     <div class="card-body text-center">
                         <span class="h3">Public Repositories</span>
@@ -80,7 +131,7 @@ inquirer.prompt([
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-md-5">
+            <div class="col-xs-4">
                 <div class="card m-3  mycards">
                     <div class="card-body text-center">
                         <span class="h3">Followers</span>
@@ -91,16 +142,16 @@ inquirer.prompt([
             </div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-12 col-md-5">
+            <div class="col-xs-4">
                 <div class="card m-3  mycards">
                     <div class="card-body text-center">
                         <span class="h3">GitHub Stars</span>
                         <br>
-                        <span class="h3" id="numStars">${public_gists}</span>
+                        <span class="h3" id="numStars">${totalStars}</span>
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-md-5">
+            <div class="col-xs-4">
                 <div class="card m-3  mycards">
                     <div class="card-body text-center">
                         <span class="h3">Following</span>
@@ -114,10 +165,33 @@ inquirer.prompt([
 
 </html>
         `; 
-        writeFileAsynch("resume.html", resumeContent).then (function () { 
-            console.log("File successfully written"); 
+        const htmlname = answer.githubname + ".html"; 
+        writeFileAsynch(htmlname, resumeContent).then (function () { 
+            console.log(`File ${htmlname} successfully written`); 
         });  
+
+        const pdfname = "./" + answer.githubname + ".pdf"; 
+        const conversion = convertFactory({
+             converterPath: convertFactory.converters.PDF
+        });
+        conversion({html: resumeContent }, function(err, result) {
+            if (err) {
+                return console.error(err); 
+            }
+            console.log(result.numberOfPages); 
+            console.log(result.logs); 
+            result.stream.pipe(fs.createWriteStream(pdfname));
+            conversion.kill(); 
+        })
+
+//         var options = { format: 'Letter',
+//                         orientation: 'portrait',
+//  }; 
+//         pdfpkg.create(resumeContent, options).toFile(pdfname, function(err, res) {
+//              if (err) return console.log(err); 
+//              console.log(res); 
+//         })
    
 });  // end of prompt answer function 
 }); 
-//do something with the writeFileAsynch? 
+ 
